23e4a5c760fd18e7db494418ffd7034de7e3799f
satinder <satindersingh472@gmail.com>
2022-08-11 19:27:45 -0600

    finished

--- src/components/templateFun.vue
+++ src/components/templateFun.vue
@@ -14,11 +14,7 @@ export default {
     },
     methods:{
         set_show(){
-            if(this.show_post === true){
-                this.show_post = false;
-            } else if(this.show_post === false){
-                this.show_post = true;
-            }
+          this.show_post = !this.show_post;
         }
     },
     data() {
@@ -26,7 +22,7 @@ export default {
             blog_content: `This is the content for the blog`,
             blog_title : `A Title`,
             blog_image_url: `https://images.unsplash.com/photo-1659806095284-8c9d1ccfc7ef?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDd8NnNNVmpUTFNrZVF8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60`,
-            show_post: true
+            show_post: false
 
         }
     },
