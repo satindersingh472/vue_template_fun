d5d587e2e4fa5d57167034b65554903a34883cf7
satinder <satindersingh472@gmail.com>
2022-08-11 19:59:14 -0600

    finished

--- src/components/templateFun.vue
+++ src/components/templateFun.vue
@@ -1,37 +1,47 @@
 <template>
   <div>
     <button @click="set_show">show</button>
-<h1>{{blog_title}}</h1>
-<p v-if="show_post === true">{{blog_content}}</p>
-<img :src="blog_image_url" alt="random_image"/>
+    <div v-if="show_post === true">
+    <div v-for="(post,index) in posts" :key="index">
+        <h1>{{post[`blog_title`]}}</h1>
+        <p>{{post[`blog_content`]}}</p>
+    </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-    mounted(){
-
+  methods: {
+    set_show() {
+      this.show_post = !this.show_post;
     },
-    methods:{
-        set_show(){
-          this.show_post = !this.show_post;
-        }
-    },
-    data() {
-        return {
-            blog_content: `This is the content for the blog`,
-            blog_title : `A Title`,
-            blog_image_url: `https://images.unsplash.com/photo-1659806095284-8c9d1ccfc7ef?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDd8NnNNVmpUTFNrZVF8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60`,
-            show_post: false
+  },
+  data() {
+    return {
+      posts: [
+        {
+          blog_content: `This is the content_one for the blog`,
+          blog_title: `Title_one`,
+        },
+        {
+          blog_content: `This is the content_two for the blog`,
+          blog_title: `Title_two`,
+        },
+        {
+          blog_content: `This is the content_three for the blog`,
+          blog_title: `Title_three`,
+        },
+      ],
 
-        }
-    },
-
+      show_post: false,
+    };
+  },
 };
 </script>
 
 <style scoped>
-div{
-    text-align: center;
+div {
+  text-align: center;
 }
 </style>
