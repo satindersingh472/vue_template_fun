3a43d07747c7db5b58e3bdcbe3aafff93b8c1907
satinder <satindersingh472@gmail.com>
2022-08-11 19:07:55 -0600

    finished

--- src/components/templateFun.vue
+++ src/components/templateFun.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
+    <button @click="set_show">show</button>
 <h1>{{blog_title}}</h1>
-<p>{{blog_content}}</p>
+<p v-if="show_post === true">{{blog_content}}</p>
 <img :src="blog_image_url" alt="random_image"/>
   </div>
 </template>
@@ -12,13 +13,21 @@ export default {
 
     },
     methods:{
-
+        set_show(){
+            if(this.show_post === true){
+                this.show_post = false;
+            } else if(this.show_post === false){
+                this.show_post = true;
+            }
+        }
     },
     data() {
         return {
             blog_content: `This is the content for the blog`,
             blog_title : `A Title`,
-            blog_image_url: `https://images.unsplash.com/photo-1659806095284-8c9d1ccfc7ef?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDd8NnNNVmpUTFNrZVF8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60`
+            blog_image_url: `https://images.unsplash.com/photo-1659806095284-8c9d1ccfc7ef?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHx0b3BpYy1mZWVkfDd8NnNNVmpUTFNrZVF8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60`,
+            show_post: true
+
         }
     },
 
@@ -26,4 +35,7 @@ export default {
 </script>
 
 <style scoped>
+div{
+    text-align: center;
+}
 </style>
